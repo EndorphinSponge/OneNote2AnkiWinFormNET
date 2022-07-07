cb7bd9115080da42b69a0dba09ce67915112de47
EndorphinSponge <hqiu1@ualberta.ca>
2022-07-07 16:33:34 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace OneNote2AnkiWinFormNET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(32, 35);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(748, 630);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(809, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 133);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Process page(s)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(809, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 124);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generate XML only";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(810, 618);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 44);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "2.3";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(834, 578);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Script version";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(809, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(260, 118);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Run Python only";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(288F, 288F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1629, 1086);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.Office.Interop.OneNote;
+
+namespace OneNote2AnkiWinFormNET
+{
+    public partial class Form1 : Form
+    {
+        // Make global OneNote instance
+        Microsoft.Office.Interop.OneNote.Application onApplication = new Microsoft.Office.Interop.OneNote.Application(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            // Load XML hierarchy
+            string hierarchy_str = "";
+            onApplication.GetHierarchy(null,
+                Microsoft.Office.Interop.OneNote.HierarchyScope.hsPages, out hierarchy_str);
+            XmlDocument hierarchy_xml_doc = new XmlDocument();
+            hierarchy_xml_doc.LoadXml(hierarchy_str);
+
+
+            // Add the root node's children to the TreeView.
+            treeView1.Nodes.Clear();
+            AddTreeViewChildNodes(treeView1.Nodes, hierarchy_xml_doc.DocumentElement);
+        }
+
+
+        // Add the children of this XML node 
+        // to this child nodes collection.
+        private void AddTreeViewChildNodes(TreeNodeCollection parent_nodes, XmlNode xml_node)
+        {
+            foreach (XmlNode child_node in xml_node.ChildNodes)
+            {
+                // Make the new TreeView node.
+                if (child_node.Attributes["ID"] != null && child_node.Attributes["name"] != null) 
+                {
+                    TreeNode new_node = parent_nodes.Add(child_node.Attributes["ID"].Value, child_node.Attributes["name"].Value);
+                    // Recursively make this node's descendants.
+                    AddTreeViewChildNodes(new_node.Nodes, child_node);
+
+                    // If this is a leaf node, make sure it's visible.
+                    //if (new_node.Nodes.Count == 0) new_node.EnsureVisible();
+                }
+
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Load required files            
+            string page_path = @"C:\Users\steve\OneDrive - ualberta.ca\Coding\OneNote2AnkiPython\Export.xml"; // Placeholder for page XML export, Python scripts refer to the same directory
+
+            // Tree parsing
+            List<TreeNode> selected_nodes = new List<TreeNode>();
+            FindCheckedNodes(selected_nodes, treeView1.Nodes);
+            foreach (TreeNode node in selected_nodes)
+            {
+                MessageBox.Show($"Found {node.Text}, {node.Name}");
+                String page_xml_str = ""; // Reset the string variable, unsure if necessary 
+                onApplication.GetPageContent($"{node.Name}", out page_xml_str, PageInfo.piBinaryData);
+                XmlDocument page_xml_doc = new XmlDocument();
+                page_xml_doc.LoadXml(page_xml_str);
+                page_xml_doc.Save(page_path); 
+                runPython();
+            }
+            
+            //XmlNodeList xn_list = hierarchy_xml_doc.SelectNodes($"//*[@name='{}']");
+            //MessageBox.Show("Executing main Python process");
+            
+
+        }
+
+        private void FindCheckedNodes(List<TreeNode> checked_nodes, TreeNodeCollection nodes)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                // Add this node.
+                if (node.Checked) checked_nodes.Add(node);
+
+                // Check the node's descendants.
+                FindCheckedNodes(checked_nodes, node.Nodes);
+            }
+        }
+
+        public void runPython()
+        {
+            //MessageBox.Show("Run Script here");
+            // New process
+            var psi = new ProcessStartInfo();
+            psi.FileName = @"C:\Users\steve\.conda\envs\venv2\python.exe";
+
+            // Script and arguments 
+            string version = textBox1.Text;
+            var script = $@"C:\Users\steve\OneDrive - ualberta.ca\Coding\OneNote2AnkiPython\Version {version}.py";
+            var file_name = "Export.xml";
+
+            psi.Arguments = $"\"{script}\" {file_name}"; // File name should not be enclosed in brackets since it is taken literally in the file name
+
+            // Process configuration & Running
+            psi.UseShellExecute = false; // For some reason, this needs to be true in order for Python script to work, maybe b/c Python program relies on Shell
+            using (var process = Process.Start(psi))
+            {
+                process.WaitForExit();
+                MessageBox.Show("Python process finished");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Load required files            
+            string page_path = @"C:\Users\steve\OneDrive - ualberta.ca\Coding\OneNote2AnkiPython\Export.xml"; // Placeholder for page XML export, Python scripts refer to the same directory
+
+            // Tree parsing
+            List<TreeNode> selected_nodes = new List<TreeNode>();
+            FindCheckedNodes(selected_nodes, treeView1.Nodes);
+            foreach (TreeNode node in selected_nodes)
+            {
+                MessageBox.Show($"Found {node.Text}, {node.Name}");
+                String page_xml_str = ""; // Reset the string variable, unsure if necessary 
+                onApplication.GetPageContent($"{node.Name}", out page_xml_str, PageInfo.piBinaryData);
+                XmlDocument page_xml_doc = new XmlDocument();
+                page_xml_doc.LoadXml(page_xml_str);
+                page_xml_doc.Save(page_path);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            runPython();
+        }
+    }
+}
